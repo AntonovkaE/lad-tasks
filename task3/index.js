022570153b42b6c38602d53cb9d9d2cf605991a8
Elvira Antonovskaya <sabirova.elvira.gam@gmail.com>
2022-08-14 16:36:11 +0500

    feat: add draft of solution for task3

--- task3/index.js
+++ task3/index.js
@@ -13,5 +13,32 @@ const computerNumber = getRandomInt(999999)
 console.log(computerNumber)
 
 readlineSync.setDefaultOptions({limit: [/\D/]})
-let numInt = readlineSync.questionInt('Введите число')
-console.log(numInt)
+let yourInt;
+let wrongPlace = 0;
+let rightNumbers = [];
+
+while (computerNumber !== yourInt) {
+  yourInt = readlineSync.questionInt('Введите число');
+  if (yourInt === computerNumber) {
+    console.log("Вы угадали");
+    return computerNumber
+  }
+  const arrInt = yourInt.split()
+  for (let i = 0; i<arrInt.length; i++) {
+    // индекс цифры с конца
+    let indexCharacter = arrInt.length - i;
+    // Есть ли первая цифра в числе комьютера
+    let checkingNumber = computerNumber.indexOf(arrInt[i])
+    if (checkingNumber !== -1) {
+      //Правильное ли место занимает цифра
+      if (computerNumber[indexCharacter] === arrInt[i]) {
+        rightNumbers.push(arrInt[i])
+      }
+      else {
+        wrongPlace++
+      }
+    }
+  }
+}
+
+
